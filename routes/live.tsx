ec8c84339b3c3a3a795ec27eabd09c9a95f23a86
hajin-chung <hajinchung1@gmail.com>
2023-02-17 18:35:43 +0900

    feat: navbar wrap

--- routes/live.tsx
+++ routes/live.tsx
@@ -5,12 +5,12 @@ export default function Live() {
     <>
       <Head />
       <body class="bg-gray-900 flex justify-center h-screen">
-        <main class="md:max-w-5xl w-full px-10 pt-5">
-          <div class="flex justify-between mb-10 items-end">
+        <main class="md:max-w-5xl w-full px-6 pt-5">
+          <div class="flex w-full flex-wrap mb-10 items-end">
             <a class="text-2xl text-white font-bold" href="/live">
               Live Downloads
             </a>
-            <div class="flex gap-4">
+            <div class="flex gap-4 ml-auto">
               <a
                 class="text-xl text-gray-400 hover:text-white font-bold"
                 href="/"
